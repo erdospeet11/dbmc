e3a38fbc0d1166af10edb9c3e7e770752efc75b2
Peter Erdos <erdospepe9@gmail.com>
2025-06-23 18:32:12 +0200

    Rewrote window handling

--- main.c
+++ main.c
@@ -1,157 +1,357 @@
-#include "window.h"
-#include "auth_window.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int run_main_application(const char* username) {
-    printf("=== Welcome to the Main Application ===\n");
-    printf("Logged in as: %s\n", username);
-    
-    WindowConfig config = window_config_default();
-    config.title = "Main Application - Cross-Platform Window";
-    config.width = 800;
-    config.height = 600;
-    
-    Window* window = window_create(config);
-    if (!window) {
-        printf("Failed to create main application window!\n");
-        return 1;
-    }
-    
-    if (!window_show(window)) {
-        printf("Failed to show main application window!\n");
-        window_destroy(window);
-        return 1;
-    }
-    
-    printf("Main application window created successfully!\n");
-#ifdef PLATFORM_WINDOWS
-    printf("Platform: Windows (WinAPI)\n");
-    printf("Controls: ESC to exit, click for coordinates\n");
-#elif defined(PLATFORM_X11)
-    printf("Platform: Linux/Unix (X11)\n");
-    printf("Controls: 'q' to quit, click for coordinates\n");
+#ifdef _WIN32
+    #include <windows.h>
+    #include <commctrl.h>
+    #pragma comment(lib, "comctl32.lib")
+    #pragma comment(lib, "user32.lib")
+    #pragma comment(lib, "gdi32.lib")
+#elif defined(__linux__)
+    #include <X11/Xlib.h>
+    #include <X11/Xutil.h>
+    #include <X11/keysym.h>
+    #include <unistd.h>
 #endif
-    
-    WindowEvent event;
-    int needs_redraw = 1;
-    
-    while (window_poll_events(window, &event)) {
-        switch (event.type) {
-            case WINDOW_EVENT_CLOSE:
-                printf("Main application window close requested\n");
-                goto cleanup;
-                
-            case WINDOW_EVENT_PAINT:
-                needs_redraw = 1;
-                break;
-                
-            case WINDOW_EVENT_KEY_PRESS:
-                printf("Key pressed: %d\n", event.data.key.key_code);
-#ifdef PLATFORM_WINDOWS
-                if (event.data.key.key_code == VK_ESCAPE) {
-                    goto cleanup;
-                }
-#elif defined(PLATFORM_X11)
-                if (event.data.key.key_code == 'q' || event.data.key.key_code == 'Q') {
-                    goto cleanup;
-                }
+
+// UI Control IDs
+#define ID_SQL_INPUT    1001
+#define ID_EXECUTE_BTN  1002
+#define ID_TABLES_LIST  1003
+#define ID_TABLE_VIEW   1004
+
+// Window dimensions
+#define WINDOW_WIDTH    1000
+#define WINDOW_HEIGHT   700
+
+// Global variables
+#ifdef _WIN32
+HWND g_hWnd = NULL;
+HWND g_hSqlInput = NULL;
+HWND g_hExecuteBtn = NULL;
+HWND g_hTablesList = NULL;
+HWND g_hTableView = NULL;
+HINSTANCE g_hInstance = NULL;
+#elif defined(__linux__)
+Display* g_display = NULL;
+Window g_window;
+int g_screen;
 #endif
-                break;
-                
-            case WINDOW_EVENT_MOUSE_CLICK:
-                printf("Mouse clicked at (%d, %d) - Button: %d\n", 
-                    event.data.mouse.x, event.data.mouse.y, event.data.mouse.button);
-                break;
-                
-            case WINDOW_EVENT_NONE:
-                break;
+
+char g_sqlCommand[1024] = {0};
+
+#ifdef _WIN32
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CREATE: {
+            //sql input field
+            g_hSqlInput = CreateWindowEx(
+                WS_EX_CLIENTEDGE,
+                "EDIT",
+                "",
+                WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL,
+                10, 10, 600, 100,
+                hwnd, (HMENU)ID_SQL_INPUT, g_hInstance, NULL
+            );
+
+            //execute button
+            g_hExecuteBtn = CreateWindow(
+                "BUTTON",
+                "Execute SQL",
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                620, 10, 100, 30,
+                hwnd, (HMENU)ID_EXECUTE_BTN, g_hInstance, NULL
+            );
+
+            //tables list
+            g_hTablesList = CreateWindow(
+                "LISTBOX",
+                "",
+                WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL,
+                10, 120, 200, 500,
+                hwnd, (HMENU)ID_TABLES_LIST, g_hInstance, NULL
+            );
+
+            //TODO: load the tables from disk
+            //sample tables
+            SendMessage(g_hTablesList, LB_ADDSTRING, 0, (LPARAM)"products");
+            SendMessage(g_hTablesList, LB_ADDSTRING, 0, (LPARAM)"users");
+            SendMessage(g_hTablesList, LB_ADDSTRING, 0, (LPARAM)"orders");
+
+            //table view
+            InitCommonControls();
+            g_hTableView = CreateWindow(
+                WC_LISTVIEW,
+                "",
+                WS_CHILD | WS_VISIBLE | WS_BORDER | LVS_REPORT,
+                220, 120, 750, 500,
+                hwnd, (HMENU)ID_TABLE_VIEW, g_hInstance, NULL
+            );
+
+            ListView_SetExtendedListViewStyle(g_hTableView, LVS_EX_GRIDLINES | LVS_EX_FULLROWSELECT);
+
+            //add columns to table view
+            LVCOLUMN lvc = {0};
+            lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+            lvc.cx = 100;
+            lvc.pszText = "Column 1";
+            ListView_InsertColumn(g_hTableView, 0, &lvc);
+            
+            lvc.pszText = "Column 2";
+            ListView_InsertColumn(g_hTableView, 1, &lvc);
+            
+            lvc.pszText = "Column 3";
+            ListView_InsertColumn(g_hTableView, 2, &lvc);
+
+            //TODO: load columns data from disk
+            //sample data
+            LVITEM lvi = {0};
+            lvi.mask = LVIF_TEXT;
+            lvi.iItem = 0;
+            lvi.iSubItem = 0;
+            lvi.pszText = "Row 1 Col 1";
+            ListView_InsertItem(g_hTableView, &lvi);
+            ListView_SetItemText(g_hTableView, 0, 1, "Row 1 Col 2");
+            ListView_SetItemText(g_hTableView, 0, 2, "Row 1 Col 3");
+
+            return 0;
         }
+
+        case WM_COMMAND: {
+            switch (LOWORD(wParam)) {
+                case ID_EXECUTE_BTN: {
+                    GetWindowText(g_hSqlInput, g_sqlCommand, sizeof(g_sqlCommand));
+                    MessageBox(hwnd, g_sqlCommand, "SQL Command", MB_OK | MB_ICONINFORMATION);
+                    SetWindowText(g_hSqlInput, "");
+                    break;
+                }
+                
+                case ID_TABLES_LIST: {
+                    if (HIWORD(wParam) == LBN_SELCHANGE) {
+                        int sel = SendMessage(g_hTablesList, LB_GETCURSEL, 0, 0);
+                        if (sel != LB_ERR) {
+                            char tableName[256];
+                            SendMessage(g_hTablesList, LB_GETTEXT, sel, (LPARAM)tableName);
+                            
+                            char msg[512];
+                            sprintf(msg, "Selected table: %s", tableName);
+                            SetWindowText(hwnd, msg);
+                        }
+                    }
+                    break;
+                }
+            }
+            return 0;
+        }
+
+        case WM_SIZE: {
+            int width = LOWORD(lParam);
+            int height = HIWORD(lParam);
+            
+            if (g_hTableView) {
+                SetWindowPos(g_hTableView, NULL, 220, 120, width - 240, height - 140, 
+                           SWP_NOZORDER);
+            }
+            return 0;
+        }
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+int CreateWindowsWindow() {
+    const char* className = "DBMCWindow";
+    
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = g_hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = className;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    g_hWnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        className,
+        "Database Management Client",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL, NULL, g_hInstance, NULL
+    );
+
+    if (g_hWnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(g_hWnd, SW_SHOWDEFAULT);
+    UpdateWindow(g_hWnd);
+
+    return 1;
+}
+
+#elif defined(__linux__)
+void DrawUI() {
+    XClearWindow(g_display, g_window);
+    
+    XDrawRectangle(g_display, g_window, DefaultGC(g_display, g_screen), 10, 10, 600, 100);
+    XDrawRectangle(g_display, g_window, DefaultGC(g_display, g_screen), 620, 10, 100, 30);
+    XDrawRectangle(g_display, g_window, DefaultGC(g_display, g_screen), 10, 120, 200, 500);
+    XDrawRectangle(g_display, g_window, DefaultGC(g_display, g_screen), 220, 120, 750, 500);
+
+    XDrawString(g_display, g_window, DefaultGC(g_display, g_screen), 10, 130, "Tables:", 7);
+    XDrawString(g_display, g_window, DefaultGC(g_display, g_screen), 220, 130, "Table Data:", 11);
+    XDrawString(g_display, g_window, DefaultGC(g_display, g_screen), 620, 25, "Execute", 7);
+    
+    XFlush(g_display);
+}
+
+int CreateX11Window() {
+    g_display = XOpenDisplay(NULL);
+    if (g_display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        return 0;
+    }
+
+    g_screen = DefaultScreen(g_display);
+    
+    g_window = XCreateSimpleWindow(
+        g_display,
+        RootWindow(g_display, g_screen),
+        10, 10,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        1,
+        BlackPixel(g_display, g_screen),
+        WhitePixel(g_display, g_screen)
+    );
+
+    XSelectInput(g_display, g_window, 
+                 ExposureMask | KeyPressMask | ButtonPressMask | StructureNotifyMask);
+
+    XMapWindow(g_display, g_window);
+    XStoreName(g_display, g_window, "Database Management Client");
+
+    return 1;
+}
+
+void HandleX11Events() {
+    XEvent event;
+    
+    while (1) {
+        XNextEvent(g_display, &event);
         
-        if (needs_redraw) {
-            window_clear(window);
+        switch (event.type) {
+            case Expose:
+                if (event.xexpose.count == 0) {
+                    DrawUI();
+                }
+                break;
+                
+            case KeyPress: {
+                KeySym key = XLookupKeysym(&event.xkey, 0);
+                if (key == XK_Escape) {
+                    return;
+                }
+                break;
+            }
             
-            char welcome_msg[256];
-            snprintf(welcome_msg, sizeof(welcome_msg), "Welcome %s! Main Application Running", username);
-            window_draw_text(window, 50, 50, welcome_msg);
+            case ButtonPress: {
+                int x = event.xbutton.x;
+                int y = event.xbutton.y;
+                
+                if (x >= 620 && x <= 720 && y >= 10 && y <= 40) {
+                    printf("Execute button clicked\n");
+                }
+                else if (x >= 10 && x <= 210 && y >= 120 && y <= 620) {
+                    printf("Tables list clicked\n");
+                }
+                break;
+            }
             
-            window_draw_text(window, 50, 80, "This is the main application window");
-            window_draw_text(window, 50, 110, "You have successfully authenticated!");
-            
-#ifdef PLATFORM_WINDOWS
-            window_draw_text(window, 50, 140, "Running on Windows - Press ESC to exit");
-#elif defined(PLATFORM_X11)
-            window_draw_text(window, 50, 140, "Running on Linux/Unix - Press 'q' to quit");
-#endif
-            
-            window_draw_text(window, 50, 170, "Click anywhere to see coordinates");
-            
-            window_draw_rectangle(window, 200, 200, 100, 50, 0xFF0000);
-            
-            window_draw_rectangle(window, 320, 200, 100, 50, 0x00FF00);
-            
-            window_draw_rectangle(window, 440, 200, 100, 50, 0x0000FF);
-            
-            char user_info[256];
-            snprintf(user_info, sizeof(user_info), "Authenticated User: %s", username);
-            window_draw_text(window, 50, 300, user_info);
-            window_draw_text(window, 50, 330, "Application features unlocked!");
-            
-            window_present(window);
-            needs_redraw = 0;
+            case ClientMessage:
+                return;
         }
-
-#ifdef PLATFORM_WINDOWS
-        Sleep(1);
-#elif defined(PLATFORM_X11)
-        usleep(1000);
-#endif
     }
-    
-cleanup:
-    printf("Cleaning up main application and exiting...\n");
-    window_destroy(window);
+}
+#endif
+
+int InitializeWindow() {
+#ifdef _WIN32
+    g_hInstance = GetModuleHandle(NULL);
+    return CreateWindowsWindow();
+#elif defined(__linux__)
+    return CreateX11Window();
+#else
+    fprintf(stderr, "Unsupported platform\n");
     return 0;
+#endif
+}
+
+void RunEventLoop() {
+#ifdef _WIN32
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+#elif defined(__linux__)
+    HandleX11Events();
+#endif
+}
+
+void Cleanup() {
+#ifdef _WIN32
+#elif defined(__linux__)
+    if (g_display) {
+        XDestroyWindow(g_display, g_window);
+        XCloseDisplay(g_display);
+    }
+#endif
 }
 
 int main() {
-    printf("=== Application Startup - Authentication Required ===\n");
-    
-    AuthWindow* auth_win = auth_window_create("Login Required - Main Application");
-    if (!auth_win) {
-        printf("Failed to create authentication window!\n");
+    printf("Database Management Client\n");
+    printf("Platform: ");
+#ifdef _WIN32
+    printf("Windows\n");
+#elif defined(__linux__)
+    printf("Linux\n");
+#else
+    printf("Unknown\n");
+#endif
+
+    if (!InitializeWindow()) {
+        fprintf(stderr, "Failed to initialize window\n");
         return 1;
     }
+
+    printf("Window created successfully\n");
+    printf("Controls:\n");
+    printf("- SQL Input Field: Enter SQL commands\n");
+    printf("- Execute Button: Run SQL commands\n");
+    printf("- Tables List: View available tables\n");
+    printf("- Table View: Display query results\n");
+#ifdef __linux__
+    printf("- ESC key: Exit application\n");
+#endif
+
+    RunEventLoop();
     
-    if (!auth_window_show(auth_win)) {
-        printf("Failed to show authentication window!\n");
-        auth_window_destroy(auth_win);
-        return 1;
-    }
-    
-    printf("Please login to access the main application...\n");
-    
-    int auth_result = auth_window_run(auth_win);
-    
-    if (auth_result == 1) {
-        char username[MAX_INPUT_LENGTH];
-        char password[MAX_INPUT_LENGTH];
-        
-        if (auth_window_get_credentials(auth_win, username, password, 
-                                      sizeof(username), sizeof(password))) {
-            printf("\n=== Authentication Successful ===\n");
-            printf("Welcome, %s!\n", username);
-            
-            auth_window_destroy(auth_win);
-            
-            return run_main_application(username);
-        } else {
-            printf("Failed to retrieve credentials!\n");
-            auth_window_destroy(auth_win);
-            return 1;
-        }
-    } else {
-        printf("\nAuthentication cancelled or failed.\n");
-        printf("Access denied - exiting application.\n");
-        auth_window_destroy(auth_win);
-        return 1;
-    }
-}
+    Cleanup();
+    return 0;
+} 
